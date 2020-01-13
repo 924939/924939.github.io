d5d952bf9b3c0b5d739670e15bc143dd0528915f
924939 <924939@qq.com>
2020-01-13 14:45:16 +0800

    1

--- js/common.js
+++ js/common.js
@@ -11,28 +11,42 @@ window.onresize = function(){
     }
 };
 
-function is_weixin(){
-    let ua = navigator.userAgent.toLowerCase();
-    if(ua.match(/MicroMessenger/i)=="micromessenger") {
-        return true;
-    } else {
-        return false;
-    }
-};
+// function is_weixin(){
+//     let ua = navigator.userAgent.toLowerCase();
+//     if(ua.match(/MicroMessenger/i)=="micromessenger") {
+//         return true;
+//     } else {
+//         return false;
+//     }
+// };
 
-let url = 'https://open.weixin.qq.com/connect/oauth2/authorize?appid=wxdf3f22ebfe96b912&redirect_uri=xxx&response_type=code&scope=snsapi_base&state=hyxt#wechat_redirect';
+// let wxUrl = 'https://open.weixin.qq.com/connect/oauth2/authorize?appid=wxdf3f22ebfe96b912&redirect_uri=xxx&response_type=code&scope=snsapi_base&state=hyxt#wechat_redirect';
 
-if (!is_weixin()) { 
-    window.location.href = url;
-};
+// if (!is_weixin()) { 
+//     window.location.href = wxUrl;
+// };
 
 $(function(){
-    $('.payment-btn').on('click',function(){
+
+    $('.point-focus').on('keyup',function () {
+        var val = $(this).val();
+        $(this).val($(this).val().replace(".","$#$").replace(/\./g, "").replace("$#$", ".").replace(/^(\-)*(\d+)\.(\d\d).*$/,'$1$2.$3').replace(/^(\-)*(\d+)\.(\d\d).*$/,'$1$2.$3'));
+        if (val.indexOf(".") < 0 && val!= "") {
+            $(this).val(parseFloat($(this).val()));
+        } else if (val.indexOf(".") == 0) {
+            $(this).val($(this).val().replace(/[^$#$]/g, "0.")) ;
+            $(this).val( $(this).val().replace(/\.{2,}/g, "."));
+        }
+    })      
+
+              
+        
+    $('.payment-btn button').on('click',function(){
         $('.mask').show();
         $('.popup').show();       
     })
     $('.close').on('click',function(){
         $('.mask').hide();
         $('.popup').hide();
-    });
+    }); 
 });

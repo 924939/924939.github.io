ad6a16862b562258c2e706bfe63b3ac494495590
924939 <924939@qq.com>
2020-01-13 10:58:57 +0800

    1

--- js/common.js
+++ js/common.js
@@ -1,3 +1,4 @@
+//解决键盘弹出影响定位
 let hrt = window.innerHeight;
 $('body').height(hrt);
 window.onresize = function(){
@@ -8,4 +9,9 @@ window.onresize = function(){
         let hrt = window.innerHeight;
         $('body').height(hrt);
     }
+}
+var ua = navigator.userAgent.toLowerCase();
+var isWeixin = ua.indexOf('micromessenger') != -1;
+if (!isWeixin) {
+    window.location.href = "https://open.weixin.qq.com/connect/oauth2/authorize?appid=888";
 }
